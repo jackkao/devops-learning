da3f4b48fc1df5e8f6aacf3e98f7301b99cf8b05
Jack Kao <jack_kao@outlook.com>
2022-08-23 01:35:35 +0700

    add more Terraform

---
+++ Terraform/IAM/Policies/main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_user" "admin-user" {
+    name = "ccbao"
+    tags = {
+        Description = "Project Team Lead"
+    }
+}
+
+resource "aws_iam_policy" "adminUser" {
+    name = "AdminUser"
+    policy = file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "ccbao-admin-access" {
+    user = aws_iam_user.admin-user.name
+    policy_arn = aws_iam_policy.adminUser.arn
+}
